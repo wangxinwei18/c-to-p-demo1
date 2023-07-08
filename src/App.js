27107e9a578c7f763695696b32e421d4d54b7b74
wangxinwei18 <wangxinwei18@hotmail.com>
2023-07-08 21:33:25 +0800

    update some code for demo

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import SearchBar from './components/SearchBar';
-import { useState } from 'react';
+import SearchBar from "./components/SearchBar";
+import { useState } from "react";
 
 function App() {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    const handleSubmit = (term) => {  // 传给子组件函数，term是子组件回传给父组件参数
-        // 使用useState数组不可以直接赋值给data，赋值新的updateData，再使用setData
-        let updateData = [...data, term];
-        setData(updateData);
-    };
+  const handleSubmit = (term) => {
+    // 传给子组件函数，term是子组件回传给父组件参数
+    // 使用useState数组不可以直接赋值给data，赋值新的updateData，再使用setData
+    let updateData = [...data, term];
+    setData(updateData);
+  };
 
-    const renderedData = data.map((value, index) => {
-        return <li key={index}>{value}</li>
-    });
+  const renderedData = data.map((value, index) => {
+    return <li key={index}>{value}</li>;
+  });
 
-    return (
-        <div>
-            <label>Input string/number/character</label>
-            <SearchBar onSubmit={handleSubmit} />
-            {/* {console.log(data)} */}
-            {/* 数组数据按照每个元素一行逐列显示 */}
-            <ul>
-                {renderedData}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <label>Input string/number/character</label>
+      <SearchBar onSubmit={handleSubmit} />
+      {/* {console.log(data)} */}
+      {/* 数组数据按照每个元素一行逐列显示 */}
+      <ul>{renderedData}</ul>
+    </div>
+  );
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,24 +1,26 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function SearchBar({ onSubmit }) {
-    const [term, setTerm] = useState('');
+  const [term, setTerm] = useState("");
 
-    const handleFromSubmit = (event) => {   //form 提交函数
-        event.preventDefault();
-        onSubmit(term)       // 调用父组件传过来的函数和参数
-    };
+  const handleFromSubmit = (event) => {
+    //form 提交函数
+    event.preventDefault();
+    onSubmit(term); // 调用父组件传过来的函数和参数
+  };
 
-    const handleChange = (event) => {     // input 提交函数
-        setTerm(event.target.value);
-    };
+  const handleChange = (event) => {
+    // input 提交函数
+    setTerm(event.target.value);
+  };
 
-    return (
-        <div>
-            <form onSubmit={handleFromSubmit} >
-                <input value={term} onChange={handleChange} />
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <form onSubmit={handleFromSubmit}>
+        <input value={term} onChange={handleChange} />
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
